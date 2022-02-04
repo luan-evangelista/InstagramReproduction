68a2271edbc75940d789103bd15714341176c16e
Luan_Vieira <luan.vieira@tron.com.br>
2022-02-04 00:23:55 -0300

    Adicionando Repositório: header + stories

---
+++ data/users.js
@@ -0,0 +1,37 @@
+export const USERS = [
+    {
+        user: 'Lucas',
+        image:
+        'https://segredosdomundo.r7.com/wp-content/uploads/2015/08/destaque36.jpg'
+    },
+    {
+        user: 'Botania',
+        image:
+        'https://assets.papodehomem.com.br/2019/11/22/14/24/27/ed724cee-8ea2-4b46-abee-161c92d79a1f/michaelbj-jpg'
+    },
+    {
+        user: 'Mariza',
+        image:
+        'https://www.magnaferreira.com.br/fotos/images/formato%20de%20rosto%20quadrado.jpeg'
+    },
+    {
+        user: 'Raul',
+        image:
+        'https://uploads.metropoles.com/wp-content/uploads/2019/09/26130629/Screenshot_8767.jpg'
+    },
+    {
+        user: 'Paulo',
+        image:
+        'https://conteudo.imguol.com.br/c/entretenimento/76/2021/10/18/gusttavo-lima-mostra-mais-de-10-comprimidos-que-toma-diariamente-1634591327136_v2_3x4.jpg'
+    },
+    {
+        user: 'João',
+        image:
+        'https://portalpopline.com.br/wp-content/uploads/2021/12/whindersson-nunes-2.jpg'
+    },
+    {
+        user: 'Maria',
+        image:
+        'https://veja.abril.com.br/wp-content/uploads/2018/12/ivete-sangalo-2018-3456.jpg'
+    },
+]
