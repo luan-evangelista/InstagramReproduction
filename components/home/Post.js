46a398553177fb39c3ee318120ec221b6eb13129
Kariny Neves Parreira de Vasconcelos <kariny.neves@tron.com.br>
2022-02-08 02:25:43 -0300

    Add Posts

---
+++ components/home/Post.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Divider } from 'react-native-elements';
+
+const Post = ({ post }) => {
+    return (
+        <View style={{ marginBottom: 30 }} >
+            <Divider width={1} orientation='vertical' />
+            <PostHeader post={post} />
+            <PostImage post={post} />
+        </View>
+    )
+}
+
+const PostHeader = ({ post }) => (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 5, alignItems: 'center' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }} >
+            <Image source={{ uri: post.profile_picture }} style={styles.post} />
+            <Text style={{ color: 'white', marginLeft: 5, fontWeight: '700' }}>
+                {post.user}
+            </Text>
+        </View>
+
+        <Text style={{ color: 'white', fontWeight: '900' }}>...</Text>
+    </View>
+)
+
+const PostImage = ({ post }) => (
+    <View style={{
+        width: '100%',
+        height: 450,
+    }}>
+        <Image
+        source={{ uri: post.imageUrl }}
+        style={{ height: '100%', resizeMode: 'cover' }}
+    />
+    </View>
+)
+
+const styles = StyleSheet.create({
+    post: {
+        width: 35,
+        height: 35,
+        borderRadius: 50,
+        marginLeft: 6,
+        borderWidth: 1.6,
+        borderColor: '#ff8501',
+    },
+})
+
+export default Post;
---
+++ data/posts.js
@@ -0,0 +1,208 @@
+import {USERS} from './users';
+
+export const POSTS = [
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[0].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[0].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[1].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[1].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[2].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[2].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[3].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[3].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[4].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[4].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[5].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[5].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[6].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[6].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[6].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[6].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[6].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[6].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[6].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[6].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[6].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[6].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/originals/7f/6b/93/7f6b9321b9304d7bcc71ca7560d0af64.jpg',
+        user: USERS[6].user,
+        likes: 7870,
+        caption: 'Hello, how are you?',
+        profile_picture: USERS[6].image,
+        coments:[
+            {
+                user: 'Vovozona',
+                coment: 'Woooooooooooooow!!!! This is animal !!!'
+            },
+            {
+                user: 'Titia Cotinha',
+                coment: 'MDS!!!! This is fantastic !!!'
+            },
+        ],
+    },
+]
